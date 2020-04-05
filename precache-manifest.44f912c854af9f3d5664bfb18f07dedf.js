f2bf8b456a0e4bdc2c9727e339f3d821d599ebfa
wasiak93 <kacperjacekwasiak@gmail.com>
2020-04-05 18:32:19 +0200

    Updates

---
+++ precache-manifest.44f912c854af9f3d5664bfb18f07dedf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4075573f45e379f53d7d2c9f68f404",
+    "url": "/map/index.html"
+  },
+  {
+    "revision": "0a03963b7097597be5ab",
+    "url": "/map/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2133297e3d532c7c2ccf",
+    "url": "/map/static/css/main.4aa50788.chunk.css"
+  },
+  {
+    "revision": "0a03963b7097597be5ab",
+    "url": "/map/static/js/2.ef1c8f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/map/static/js/2.ef1c8f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2133297e3d532c7c2ccf",
+    "url": "/map/static/js/main.68a3642b.chunk.js"
+  },
+  {
+    "revision": "867434ba4f4b442a59bb",
+    "url": "/map/static/js/runtime-main.b16594a8.js"
+  },
+  {
+    "revision": "8ea6aeab8d08fc9d04790391e8cf6ce7",
+    "url": "/map/static/media/mapbox-icon.8ea6aeab.bin"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/map/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/map/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/map/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/map/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
